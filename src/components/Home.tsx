4dded11c9d8a4f6e273bb179045a20ef68362b20
Bojan Bosnić <98858197+bojanbosnic@users.noreply.github.com>
2024-04-29 00:55:15 +0200

    feat(Home): install shadecn and create HomePage component

---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="h1">
+        Welcome to the <span className="font-medium">Frontend Quiz!</span>
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
